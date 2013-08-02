715ab4f6f1d3df2820198f6917ac491865423023
Spiros Martzoukos <spiros@dailysecret.com>
2013-08-02 15:47:20 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 equal-break
 ===========
 
-It break a long single line of text into two lines of equal width, by inserting a &lt;br> in the middle of the string.
+A jQuery plugin that breaks a long string into multiple equal lines of text
+
+Usage:
+$( selector ).equalBreak( options );
+
+Options:
+- lines: you can set up the number of lines to break the text to (default: 2)
---
+++ jquery.equalBreak.js
@@ -0,0 +1,49 @@
+/*
+ * jQuery Equal Line Breaking v1.0
+ * A jQuery plugin that breaks a long string into multiple equal lines of text
+ * Copyright (c) 2013 Spiros Martzoukos
+ *
+ * https://github.com/martzoukos/equal-break
+ *
+ * Depends:
+ *   - jQuery 1.3+
+
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+*/
+(function( $ ) {
+
+	$.fn.equalBreak = function( options ) {
+
+		var settings = $.extend({
+			lines: 2
+		}, options);
+
+		return this.each(function(){
+
+			var $el 				= $(this),
+					text 				= $el.html(),
+					words 			= text.split(" "),
+					breakPoints = parseInt( (text.length / settings.lines), 10),
+					breakIncr		= breakPoints,
+					letterCount = 0,
+					result			= "";
+
+    	for (i=0; i<words.length; i++) {
+    		if ( letterCount >= breakPoints ) {
+    		console.log( result );
+    			result 			+= "<br>";
+    			breakPoints += breakIncr;
+    		}
+    		result 			+= " " + words[i];
+    		letterCount += words[i].length + 1;
+    	}
+
+    	$el.html( result );
+
+		});
+
+	};
+
+}( jQuery ));
